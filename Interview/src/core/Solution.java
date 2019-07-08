a96d4910a7f4670e6892bea544a532dd50b3e077
ummabdullah <amaturrahman@gmail.com>
2019-07-08 08:53:02 -0400

    added java version remove duplicates

--- Interview/src/core/Solution.java
+++ Interview/src/core/Solution.java
@@ -2,6 +2,7 @@ package core;
 
 import java.util.LinkedHashSet;
 //import static ch.lambdaj.Lambda.selectDistinct;
+import java.util.TreeSet;
 
 public class Solution {
 
@@ -9,7 +10,9 @@ public class Solution {
 		Integer[] arr = {0,1,28,839,2,2,2,3,4,4,2,4,1,0,-8,389,-38};
 		
 		removeDupInIntArray(arr);		
+		sortArray(arr);
 		//List<Integer> numbers =  Arrays.asList(1,3,4,2,1,5,6,8,8,3,4,5,13);
+		
 	}
 	
 	public static Integer[] removeDupInIntArray(Integer[] ints){
@@ -22,6 +25,15 @@ public class Solution {
 	    Integer[] intArray = (hashSet.toArray(new Integer[hashSet.size()]));
 	    return intArray;
 	    
+	}
+	
+	public static void sortArray(Integer[] ints) {
+		TreeSet<Integer[]> myTreeSet = new TreeSet<Integer[]>();
+		myTreeSet.add(ints);
+		System.out.println(myTreeSet);
+		//return(myTreeSet);
+		
+		
 	}
 	
 	
